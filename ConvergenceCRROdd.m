be5184b326a66341957a09bfbc67c9cb53827364
Matthew Fieger <fieger.matthew@gmail.com>
2014-12-06 17:17:33 -0500

    Even Odd

---
+++ ConvergenceCRROdd.m
@@ -0,0 +1,12 @@
+function ConvergenceCRREven(S,K,r,T,sigma,q,N,IsCall)
+	% Calculate the continuous time price using the BSM Model
+	black_scholes_price = BSPrice(S,K,T,r,sigma,q,IsCall);
+	LatticeC = zeros(1,N/2);
+	% Calculate the CRR discrete approximation for various timesteps
+	for i = (1:N/2)
+		LatticeC(i) = EuroCRR(S,K,r,T,sigma,q,i*2-1,IsCall);
+	end
+	vector = (1:N/2)*2
+	plot(1:N/2, ones(1,N/2)*black_scholes_price);
+	hold on;
+	plot(1:N/2, LatticeC);
