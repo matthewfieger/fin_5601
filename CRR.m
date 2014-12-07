2d71b84f88f28b022b835637778d791260edcf12
Matthew Fieger <fieger.matthew@gmail.com>
2014-12-07 09:29:25 -0500

    American Options

--- CRR.m
+++ CRR.m
@@ -1,9 +1,16 @@
-function [price, lattice] = EuroCRR(S,K,r,T,sigma,q,N,IsCall)
+function [price, lattice] = EuroCRR(S,K,r,T,sigma,q,N,IsCall,IsAmer)	
 	deltaT = T/N; % Discrete time step.
 	u = exp(sigma * sqrt(deltaT)); % Up movement factor.
 	d = 1/u; % Down movement factor.
 	p = (exp((r-q)*deltaT) - d)/(u-d); % Probability of up movement.
 	lattice = zeros(N+1, N+1); % Empty lattice for our values. Must add 1 for one-based indexing.
+
+	if IsCall
+		Intrinsic = @(S) max(0,S-K);
+	else
+		Intrinsic = @(S) max(0,K-S);
+	end
+
 	% Our lattice is lattice(value,time) or lattice(y,x) if thinking visually.
 	for i = 0:N
 		% Compute intrinsic value at maturity
@@ -17,11 +24,8 @@ function [price, lattice] = EuroCRR(S,K,r,T,sigma,q,N,IsCall)
 		% value to the top node in the lattice.  It is arbitrary, but makes it a little less intuitive.
 		% So lattice(1,N+1) = lowest value final node.
 		% And lattice(N+1, N+1) = highest value final node.
-		if IsCall
-            lattice(i+1, N+1) = max(0, S*(u^i)*(d^(N-i)) - K);
-		else
-			lattice(i+1, N+1) = max(0, K - S*(u^i)*(d^(N-i)) );
-		end
+		S_t = S*(u^i)*(d^(N-i));
+		lattice(i+1, N+1) = Intrinsic(S_t);
 	end
 
 	for j = N-1:-1:0 % Moving backward in time from the second to last time step because we already calculated the final time step.
@@ -41,7 +45,13 @@ function [price, lattice] = EuroCRR(S,K,r,T,sigma,q,N,IsCall)
 			% The lowest slot to the right is lattice(N+1,N+1).  Moving backwards in time,
 			% and up one slot in the tree, we get to lattice(N-1, N).
 			% And the final iteration is lattice(1,1)
-			lattice(i+1, j+1) = exp(-r*deltaT) * (p * lattice(i+2,j+2) + (1-p) * lattice(i+1,j+2));
+			OptionVal = exp(-r*deltaT) * (p * lattice(i+2,j+2) + (1-p) * lattice(i+1,j+2));
+			if IsAmer
+				IntrinsicVal = Intrinsic(S*(u^(i))*(d^(j-i))); % Is this j-i?
+				lattice(i+1,j+1) = max(IntrinsicVal,OptionVal);
+			else
+				lattice(i+1, j+1) = OptionVal;
+			end
 		end
 	end
 	price = lattice(1,1);
